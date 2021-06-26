1ec5e7eafa584592eed9e10319fe40d7c5f9170a
nsfoliveira <nsfoliveira90@gmail.com>
2021-06-25 21:27:58 -0300

    Create tbfunc.sql

---
+++ MYSQL/SQL_Analysis/tbfunc.sql
@@ -0,0 +1,9 @@
+create table tb_func(
+id					int primary key not null,
+estado_civil		varchar(30),
+grau_instrucao		varchar(30),
+numero_filhos		int,
+salario_hora		double,
+idade				int,
+reg_procedencia		varchar(30)
+)
