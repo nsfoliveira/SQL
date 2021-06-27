8babbec4eb6ab560b37d5635fb76048edc538a10
nsfoliveira <nsfoliveira90@gmail.com>
2021-06-27 17:59:19 -0300

    Update insert_tbVendedores.sql

--- MYSQL/Proj_empresaSucos/insert_tbVendedores.sql
+++ MYSQL/Proj_empresaSucos/insert_tbVendedores.sql
@@ -1,5 +1,13 @@
-insert into tbvendedores (
 MATRICULA, NOME, PERCENTUAL_COMISSAO)
-values ('00233', 'João Geraldo da Fonsceca', 10);
+values ('00233', 'João Geraldo da Fonsceca', 0,10);
 
 select * from tbvendedores
+
+ INSERT INTO tbvendedores (
+ MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES ('00235','Márcio Almeida Silva', 0.08);
+ 
+ INSERT INTO tbvendedores
+(MATRICULA, NOME, PERCENTUAL_COMISSAO)
+VALUES
+('00236','Cláudia Morais',0.08);
