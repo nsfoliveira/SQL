3f12b20df8eaf27971b1dc4dd5c00b78103d6db8
nsfoliveira <nsfoliveira90@gmail.com>
2021-06-27 17:22:41 -0300

    Create insert_tbProdutos.sql
    Utilizando o comando INSERT INTO para inserir valores dentro da tabela de produtos, mas para não precisar fazer isso registro, por registro, farei a importação do arquivo

---
+++ MYSQL/Proj_empresaSucos/insert_tbProdutos.sql
@@ -0,0 +1,7 @@
+use empresa_sucos;
+
+select * from tbprodutos;
+
+insert into tbprodutos (
+COD_PRODUTO, NOME_PRODUTO, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA) 
+values ('1040107', 'Light - 350 ml - Melância', 'Lata', '350ml', 'Melancia', 4.56);
