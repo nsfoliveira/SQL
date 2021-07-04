62a448a256903596fa3927f4a3a95e381b290d4a
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-04 15:10:02 -0300

    Update scriptDeConsultas.sql

--- MYSQL/Proj_empresaSucos/scriptDeConsultas.sql
+++ MYSQL/Proj_empresaSucos/scriptDeConsultas.sql
@@ -7,3 +7,8 @@ SELECT NOME, MATRICULA FROM tbvendedores;
 SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro'; /*clausula que diz da onde queremos que retorne o registro, ou seja, faz um filtro*/
 
 SELECT * FROM tbvendedores WHERE PERCENTUAL_COMISSAO > 0.1;
+
+SELECT * FROM TABELA_DE_VENDEDORES WHERE YEAR(DATA_ADMISSAO) >= 2016;
+
+SELECT * FROM tbcliente WHERE (IDADE >= 28 AND CIDADE = 'RIO DE JANEIRO' AND SEXO = 'F') OR (IDADE >= 30 AND CIDADE = 'SÃO PAULO' AND SEXO = 'M');
+
