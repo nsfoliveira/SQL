3ad47d85509649abd568e95923232ff6fe70d05e
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-11 18:34:02 -0300

    Update Revisando_Consultas.sql

---
+++ MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas.sql
@@ -0,0 +1,11 @@
+use sucos_vendas;
+
+select * from itens_notas_fiscais;
+
+select * from notas_fiscais;
+
+select * from tabela_de_clientes;
+
+select * from tabela_de_produtos;
+
+select * from tabela_de_vendedores;
