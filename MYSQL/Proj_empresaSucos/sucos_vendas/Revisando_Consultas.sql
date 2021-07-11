5c8fa9a9787e69f6df799a7baa07bcb90b4d1f5c
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-11 18:48:07 -0300

    Update Revisando_Consultas.sql
    usando BETWEEN

--- MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas.sql
+++ MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas.sql
@@ -13,3 +13,5 @@ select * from tabela_de_vendedores;
 select CPF as Identificador, NOME as Cliente from tabela_de_clientes;
 
 select * from notas_fiscais where MATRICULA = 00235;
+
+select * from notas_fiscais where IMPOSTO between 0.1 and 0.13;
