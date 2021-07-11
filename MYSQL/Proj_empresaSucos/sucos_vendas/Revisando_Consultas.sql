0cc92b8ef77fd4ae1e64abc2cea0d88dc3953839
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-11 18:39:33 -0300

    Update Revisando_Consultas.sql
    filtrando com WHERE

--- MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas.sql
+++ MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas.sql
@@ -11,3 +11,5 @@ select * from tabela_de_produtos;
 select * from tabela_de_vendedores;
 
 select CPF as Identificador, NOME as Cliente from tabela_de_clientes;
+
+select * from notas_fiscais where MATRICULA = 00235;
