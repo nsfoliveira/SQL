89bca2e20edab84cb786ce06bad5987e2df1c3c5
nsfoliveira <nsfoliveira90@gmail.com>
2021-07-11 19:22:50 -0300

    Update Revisando_Consultas_Condicionais.sql

--- MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas_Condicionais.sql
+++ MYSQL/Proj_empresaSucos/sucos_vendas/Revisando_Consultas_Condicionais.sql
@@ -3,3 +3,7 @@ select * from tabela_de_produtos where SABOR = 'manga' OR EMBALAGEM = 'PET';
 select * from tabela_de_produtos where SABOR = 'manga' AND EMBALAGEM = 'PET';
 
 select * from tabela_de_produtos where SABOR = 'manga' AND not EMBALAGEM = 'PET';
+
+select * from tabela_de_produtos where SABOR in ('laranja', 'manga');
+
+select * from tabela_de_clientes where CIDADE in ('Rio de Janeiro') AND IDADE between 25 and 28;
