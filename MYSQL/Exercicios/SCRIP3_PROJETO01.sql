69175770d1871cd245e93cc65807b22041795458
nsfoliveira <nsfoliveira90@gmail.com>
2021-09-19 16:55:44 -0300

    Create SCRIP3_PROJETO01.sql
    03 FORMAR DE INSERIR DADOS NA TABELA COM INSERT INTO

---
+++ MYSQL/Exercicios/SCRIP3_PROJETO01.sql
@@ -0,0 +1,8 @@
+desc tb_cliente;
+
+insert into tb_cliente values ('NATALIA', 'NATALIA', null, 'NSFOLIVEIRA!GMAIL.COM', 'AV PRESIDENTE WILSON', '991260846', 'F');
+
+insert into tb_cliente(NOME, NOME_SOCIAL, CPF, EMAIL, ENDERECO, TELEFONE, SEXO) values ('LILIAN', 'LILIAN', NULL, 'LILIAN@GMAIL.COM', 'AV AVENIDA', '991284685', 'F');
+
+insert into tb_cliente values ('MARCO', 'MARCO', NULL, 'MARCO@GMAIL.COM', 'AV PRESIDENTE WILSON', '9758528425', 'M'),
+							  ('ANDRIELLE', 'ANDRIELLE', NULL, 'ANDRIELLE!GMAIL.COM', 'AV GUASSU', '34961715', 'F');
